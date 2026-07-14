92c5e4156994433b95ef2b7e8233755809d7f889
skraaaaa84-dot <skraaaaa84@gmail.com>
2026-07-14 11:18:24 +0900

    Add files via upload

---
+++ yohaku_v2_readfix/sw.js
@@ -0,0 +1,8 @@
+const CACHE='yohaku-v2-readfix-v1';
+self.addEventListener('install',e=>self.skipWaiting());
+self.addEventListener('activate',e=>e.waitUntil(
+  caches.keys().then(keys=>Promise.all(keys.map(k=>caches.delete(k)))).then(()=>self.clients.claim())
+));
+self.addEventListener('fetch',e=>{
+  e.respondWith(fetch(e.request).catch(()=>caches.match(e.request)));
+});
